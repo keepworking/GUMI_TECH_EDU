d589ce44be7db33d469fc959c99ad8ef75948d96
yevgeny hong <yev_science@naver.com>
2016-08-22 10:44:03 +0900

    Add files via upload

---
+++ Arduino/LedControl/LedControl.ino
@@ -0,0 +1,16 @@
+void setup() {
+  Serial.begin(9600);
+  pinMode(13,OUTPUT);
+}
+
+void loop() {
+  if(Serial.available() > 0){
+    char read = Serial.read();
+    if(read == 'A'){
+      digitalWrite(13,HIGH);
+    }
+    if(read == 'B'){
+      digitalWrite(13,LOW);
+    }
+  }
+}
