4f064245cf022e8d471dbcdee3ce0f59a8b96338
yevgeny hong <yev_science@naver.com>
2016-08-24 13:10:37 +0900

    Update smart_light.ino

--- Arduino/smart_light/smart_light.ino
+++ Arduino/smart_light/smart_light.ino
@@ -13,18 +13,14 @@ byte r = 0, g = 0, b = 0;
 int Count = 1;
 void loop() {
   if (Serial.available() > 5) { //수신받은 블루투스 데이터가 2개 초과일때
-    if(Serial.read() != '[')
-    {
-      return;
-    }
+    if(Serial.read() != '['){return;}
+    //DATA 패킷이 '['로 시작하고 ']'로 종료가 됩니다.
+    //패킷의 포멧이 맞지 않을경우에는 LED가 동작하지 않도록 되어있습니다.
     Count = Serial.read(); 
     r = Serial.read();//첫번째 데이터를 r에 저장
     g = Serial.read();//두번째 데이터를 g에 저장
     b = Serial.read();//세번째 데이터를 b에 저장
-    if(Serial.read() != ']')
-    {
-      return;
-    }
+    if(Serial.read() != ']'){return;}
     Serial.flush();//통신내용 초기화
     setAllColor(Count,pixels.Color(r, g, b));
   }
